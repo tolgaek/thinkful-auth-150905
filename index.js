7e5bf71606fe2b3b766e1b06461ebd958a8215a4
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-09-15 18:28:56 -0400

    Initialize express server

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+
+});
+
+app.get('/authenticate', function(req, res) {
+
+});
+
+app.get('/authenticate/callback', function(req, res) {
+
+});
+
+app.get('/display', function(req, res) {
+
+});
+
+
+app.listen(8060);
